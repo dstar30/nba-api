d7581f719c5c2d0fc7cda5a0ef0ca0ee011d3e68
djiang49 <djiang49@gatech.edu>
2020-03-27 20:57:45 -0500

    initial commit

---
+++ src/test/java/com/daniel/nbaapi/NbaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.nbaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
