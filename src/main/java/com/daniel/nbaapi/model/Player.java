aaff31d25a9c288efaec763dff08d55d53f23737
djiang49 <djiang49@gatech.edu>
2020-04-10 19:30:01 -0500

    add overloaded constructors

--- src/main/java/com/daniel/nbaapi/model/Player.java
+++ src/main/java/com/daniel/nbaapi/model/Player.java
@@ -1,5 +1,6 @@
 package com.daniel.nbaapi.model;
 
+import java.util.Date;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +10,33 @@ import lombok.Setter;
 public class Player {
     private final UUID id;
     private final String name;
+    private int yearStart;
+    private int yearEnd;
+    private int height;
+    private int weight;
+    private Date birthDate;
+    private String college;
 
-    public Player(UUID id, String name) {
+    public Player(UUID id, String name){
         this.id = id;
         this.name = name;
+    }
+
+    public Player(UUID id, String name, int height, int weight){
+        this.id = id;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Player(UUID id, String name, int yearStart, int yearEnd, int height, int weight, Date birthDate, String college) {
+        this.id = id;
+        this.name = name;
+        this.yearStart = yearStart;
+        this.yearEnd = yearEnd;
+        this.height = height;
+        this.weight = weight;
+        this.birthDate = birthDate;
+        this.college = college;
     }
 }
